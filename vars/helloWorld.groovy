8fcafa16d0418c4336d3271e9968a4ab6faa642c
Christian Hettlage <hettlage@saao.ac.za>
2023-05-08 13:25:26 +0200

    Fix library resource call

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,6 @@
 def call(String name) {
   echo "Hello Library ${name}!"
-  def scriptContent = loadLibraryResource 'za/ac/saao/deploy.sh'
+  def scriptContent = libraryResource 'za/ac/saao/deploy.sh'
   writeFile file: 'deploy.sh', text: scriptContent
   sh 'bash deploy.sh "Finder Chart Generator" dev.example.com'
 }
