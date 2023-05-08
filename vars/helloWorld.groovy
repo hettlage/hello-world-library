ee138c0e5a13cc0e7a60e5bdb7667642bd78a45d
Christian Hettlage <hettlage@saao.ac.za>
2023-05-08 13:57:08 +0200

    Use a map

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,8 @@
-def call(String name) {
-  echo "Hello Library ${name}!"
+def call(Map config = [:]) {
+  if (!config.containsKey("name")) {
+    error "The configuration is missing a name"
+  }
+  echo "Hello Library ${config.name}!"
   def scriptContent = libraryResource 'za/ac/saao/deploy.sh'
   writeFile file: 'deploy.sh', text: scriptContent
   sh 'bash deploy.sh "Finder Chart Generator" dev.example.com'
